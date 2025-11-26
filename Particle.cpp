a1949922e64c43ad877ab47a118c88f01a48f839
Kong-Ching <kc.sung@msa.hinet.net>
2025-11-25 21:54:53 -0800

    VS IDE plus; fix M_PI dup.

--- Particle.cpp
+++ Particle.cpp
@@ -52,8 +52,8 @@ void Particle::unitTests()
     int score = 0;
 
     cout << "Testing RotationMatrix constructor...";
-    double theta = M_PI / 4.0;
-    RotationMatrix r(M_PI / 4);
+    double theta = P_PI / 4.0;
+    RotationMatrix r(P_PI / 4);
     if (r.getRows() == 2 && r.getCols() == 2 && almostEqual(r(0, 0), cos(theta))
         && almostEqual(r(0, 1), -sin(theta))
         && almostEqual(r(1, 0), sin(theta))
@@ -116,7 +116,7 @@ void Particle::unitTests()
 
     cout << "Applying one rotation of 90 degrees about the origin..." << endl;
     Matrix initialCoords = m_A;
-    rotate(M_PI / 2.0);
+    rotate(P_PI / 2.0);
     bool rotationPassed = true;
     for (int j = 0; j < initialCoords.getCols(); j++)
     {
